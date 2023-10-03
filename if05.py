4d44b415dd4b5fe0eb366f032565ae26635f313b
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-02 23:42:32 -0400

    done by Navruzbek

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    n1=n%10
+    n2=n//10%10
+    n3=n//100%10
+    n4=n//1000%10
+    n5=n//10000
+    large=n1
+    if large<n2:
+        large=n2
+    if large<n3:
+        large=n3
+    if large<n4:
+        large=n4
+    if large<n5:
+        large=n5
+    return large
+print(main(23546))
