795989c463ff6a8996432faa6c5708cbdf688d92
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-02 23:31:56 -0400

    done by Navruzbek

--- if03.py
+++ if03.py
@@ -8,4 +8,16 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    large=a
+    small=a
+    if large<b:
+        large=b
+    if large<c:
+        large=c
+    if small>b:
+        small=b
+    if small>c:
+        small=c
+    return a+b+c-large-small
+print(main(3,7,1))
+print(main(5,5,-1))
